c89b60d52b4bf892f2689f40bb476d9cfac08f26
Jurj-Bogdan <bogdanjurj11@gmail.com>
2024-07-31 17:04:46 +0300

    issue-46
    Signed-off-by: Jurj-Bogdan <bogdanjurj11@gmail.com>

--- src/Extension/DateExtension.php
+++ src/Extension/DateExtension.php
@@ -4,11 +4,19 @@ declare(strict_types=1);
 
 namespace Dot\Twig\Extension;
 
+use DateTime;
+use DateTimeImmutable;
 use DateTimeInterface;
+use DateTimeZone;
+use Exception;
 use Twig\Environment;
 use Twig\Extension\AbstractExtension;
 use Twig\Extension\CoreExtension;
 use Twig\TwigFilter;
+
+use function ctype_digit;
+use function strtolower;
+use function substr;
 
 class DateExtension extends AbstractExtension
 {
@@ -31,15 +39,24 @@ class DateExtension extends AbstractExtension
     /**
      * Filters for converting dates to a time ago string like Facebook and Twitter has.
      * If none given, the current time will be used.
+     *
+     * @throws Exception
      */
     public function diff(
         Environment $env,
         string|DateTimeInterface|null $date,
-        string|DateTimeInterface|null $now = null
+        string|DateTimeInterface|null $now = null,
+        string|DateTimeZone|null $timezone = null,
     ): string {
+        if (null === $timezone) {
+            $timezone = $env->getExtension(CoreExtension::class)->getTimezone();
+        } elseif (! $timezone instanceof DateTimeZone) {
+            $timezone = new DateTimeZone($timezone);
+        }
+
         // Convert both dates to DateTime instances.
-        $date = $env->getExtension(CoreExtension::class)->convertDate($date);
-        $now  = $env->getExtension(CoreExtension::class)->convertDate($now);
+        $date = $this->convertDate($date, $timezone);
+        $now  = $this->convertDate($now, $timezone);
 
         // Get the difference between the two DateTime objects.
         $diff = $date->diff($now);
@@ -54,6 +71,40 @@ class DateExtension extends AbstractExtension
         }
 
         return '';
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function convertDate(string|DateTimeInterface|null $date, DateTimeZone $timezone): DateTimeInterface
+    {
+        if ($date instanceof DateTimeImmutable) {
+            return $date->setTimezone($timezone);
+        }
+
+        if ($date instanceof DateTimeInterface) {
+            $date = clone $date;
+            $date->setTimezone($timezone);
+
+            return $date;
+        }
+
+        if (null === $date || 'now' === strtolower($date)) {
+            if (null === $date) {
+                $date = 'now';
+            }
+
+            return new DateTime($date, $timezone);
+        }
+
+        if (
+            ctype_digit($date)
+            || (! empty($date) && '-' === $date[0] && ctype_digit(substr($date, 1)))
+        ) {
+            return new DateTime('@' . $date, $timezone);
+        } else {
+            return new DateTime($date, $timezone);
+        }
     }
 
     public function getPluralizedInterval(mixed $count, int $invert, string $unit): string
